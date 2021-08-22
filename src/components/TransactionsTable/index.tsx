ceb515a64ecfa1a7f5ec42efb74bdd22978555ed
Renato Marinho <renato.marinhofr@gmail.com>
2021-08-22 01:57:49 -0300

    config axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 
@@ -6,9 +7,8 @@ import { Container } from "./styles";
 export function TransactionsTable() {
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/transactions')
-    .then(response => response.json())
-    .then(data => console.log(data))
+    api.get('transactions')
+    .then(response => console.log(response.data))
   }, [])
   return (
     <Container>
